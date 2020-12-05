bb115ef0213f0a531149a3552dccbd19b7f6f086
Gnel12345 <gnel1234@yahoo.com>
2020-12-05 17:26:14 -0600

    seventh commit

--- gabler-client/src/components/HomePage.js
+++ gabler-client/src/components/HomePage.js
@@ -2,8 +2,9 @@ import React from "react";
 import {Link } from "react-router-dom";
 
 
-const HomePage = () => (
-    
+const HomePage = ({currentUser}) => {
+    if(!currentUser.isAuthenticated){
+    return(
         <div className= "home-hero">
             <h2>How Uuu Doiin?</h2>
             <h2>New to Gabler?</h2>
@@ -11,7 +12,11 @@ const HomePage = () => (
                 Sign Up Here
             </Link>
         </div>
+
+    )
     
-)
+}
+return (<div><h1>Welcome back</h1></div>)
+    }
 
 export default HomePage;
--- gabler-client/src/containers/Main.js
+++ gabler-client/src/containers/Main.js
@@ -3,24 +3,36 @@ import {Switch, Route, withRouter, Redirect} from "react-router-dom";
 import {connect} from "react-redux"
 import HomePage from "../components/HomePage";
 import AuthForm from "../components/AuthForm";
-import {authUser} from "../store/actions/auth"
+import {authUser} from "../store/actions/auth";
+import {removeError} from "../store/actions/errors";
 
 
 
 const Main = props => {
-    const {authUser} = props;
+    const {authUser, errors, removeError, currentUser} = props;
     return(
         <div className ="container">
             <Switch>
-                <Route exact path = "/" render = {props => <HomePage{...props} />}></Route>
+                <Route exact path = "/" render = {props => <HomePage currentUser={currentUser} {...props} />}></Route>
                     <Route exact path ="/signin" render = {props =>{
                             return(
-                                <AuthForm onAuth = {authUser} buttonText="Log In" heading="Welcome Back"{...props}/>
+                                <AuthForm 
+                                removeError = {removeError}
+                                errors = {errors}
+                                onAuth = {authUser}
+                                 buttonText="Log In"
+                                  heading="Welcome Back"{...props}/>
                             )
                     } }></Route>
                     <Route exact path ="/signup" render = {props =>{
                             return(
-                                <AuthForm onAuth = {authUser} signUp buttonText="Sign Up" heading="Join Gabler"{...props}/>
+                                <AuthForm 
+                                removeError={removeError}
+                                errors={errors}
+                                 onAuth = {authUser}
+                                  signUp 
+                                  buttonText="Sign Up" 
+                                  heading="Join Gabler"{...props}/>
                             )
                     } }></Route>
 
@@ -33,8 +45,9 @@ const Main = props => {
 
 function mapStateToProps(state){
     return{
-        currentUser:state.currentUser
+        currentUser:state.currentUser,
+        errors:state.errors
     }
 }
 
-export default withRouter(connect(mapStateToProps,{authUser})(Main));
+export default withRouter(connect(mapStateToProps,{authUser, removeError})(Main));
